656163752be7447a82a7e3b49421420a04fa80e5
unknown <juho.karenko@gmail.com>
2013-03-12 20:23:47 +0200

    added project structure and stub documentation

---
+++ seuss-game/src/seuss/game/SeussGame.java
@@ -0,0 +1,16 @@
+package seuss.game;
+
+
+/**
+ *
+ * @author juho karenko
+ */
+public class SeussGame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    }
+
+}
